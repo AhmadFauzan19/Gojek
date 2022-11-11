78e1a0e2a725f93dbfc8cbd98af2571bf0ff3f1a
Ahmad Fauzan <ahmadfauzan190603@gmail.com>
2022-11-11 22:41:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gojek/ui/promo/PromoFragment.kt
@@ -0,0 +1,32 @@
+package com.example.gojek.ui.promo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.gojek.databinding.FragmentChatBinding
+import com.example.gojek.databinding.FragmentPromoBinding
+
+class PromoFragment : Fragment() {
+
+    private var _binding: FragmentPromoBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPromoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
